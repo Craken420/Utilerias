451a120c624f1f5f251d624b106001c126de3c22
Luis A. Peña Zuñiga <lapena@grupomavi.com>
2019-01-28 13:05:20 -0600

    Se revirtio el nombre

--- RegEx/jsonRgx.js
+++ RegEx/jsonRgx.js
@@ -1,44 +1,44 @@
 /***
  * Glosario:
  * Intls: Intelisis
+ * Cmp: Componente
  */
 
-const nomVar = require('./variablesNombreJsonRgx')
-
 const jsonRegEx = {
-  [nomVar.nomVar.ampersand]:     /\&/g, //=> &
+  ampersand:   /\&/g,   //=> &
   //Uso check
-  [nomVar.comentarios]:          /^;.*/gm, //=> ;Comentario
-  [nomVar.campoConsecutivo]:     /^\w+\d{3}/m, //=> Campo002, SQL002
-  [nomVar.componentesIntelisis]: /\[.*?.*?[^]*?(?=\[)/g, //=> [Componente] contenidoComponente [
-  [nomVar.campoYcontinua]:       /^.*?\=<CONTINUA>|<CONTINUA>/gm, //=> SQL002 = <CONTINUA> algo <CONTINUA>
-  [nomVar.continuaAlInicio]:     /(?<=^.*?\=)<CONTINUA>/m, //=> SQL002= <CONTINUA> Algo
-  [nomVar.continuaFinal]:        /(?<=.*?)\<CONTINUA\>(\s+|)$/, //=> SQL= Algo <CONTINUA>
-  [nomVar.nombreArchivoEnRuta]:  /.*\\|.*\//, //=> Busca 'c:/user/documents/ ' hasta 'Nombre Archivo.txt'
-  [nomVar.nombreYtipoEsp]:       /.*(\/|\\)|\_MAVI.*|\.esp/gi, //De: Ruta\Achivo_FRM_MAVI.esp - Busca Achivo_FRM
-  [nomVar.saltoLineaVacio]:      /^\n[\s\t]*/gm, //=> \n\s\t Lineas vacias espacios y tabulador
+  comentarios: /^;.*/gm, //=> ;Comentario
+  campoConsecutivo:     /^\w+\d{3}/m, //=> Campo002, SQL002
+  componentesIntls: /\[.*?.*?[^]*?(?=\[)/g, //=> [Componente] contenidoComponente [
+  campoIntlsYcontinua:       /^.*?\=<CONTINUA>|<CONTINUA>/gm, //=> SQL002 = <CONTINUA> algo <CONTINUA>
+  continuaAlInicio:     /(?<=^.*?\=)<CONTINUA>/m, //=> SQL002= <CONTINUA> Algo
+  continuaFinal:        /(?<=.*?)\<CONTINUA\>(\s+|)$/, //=> SQL= Algo <CONTINUA>
+  nombreArchivoEnRuta:  /.*\\|.*\//, //=> Busca 'c:/user/documents/ ' hasta 'Nombre Archivo.txt'
+  nombreYtipoEsp: /.*(\/|\\)|\_MAVI.*|\.esp/gi, //De: Ruta\Achivo_FRM_MAVI.esp - Busca Achivo_FRM
+  saltoLineaVacio:      /^\n[\s\t]*/gm, //=> \n\s\t Lineas vacias espacios y tabulador
+  
 
 }
 
 exports.expresiones = jsonRegEx
 
 exports.crearRegEx = {
-  [nomVar.campoSinDigito]:     tipoCampo => { return  new RegExp(`^${tipoCampo}\\=.*`, `gim`)},
-  [nomVar.campoConDigito]:     tipoCampo => { return  new RegExp(`^${tipoCampo}\\d{3}\\=.*`, `gim`)},
-  [nomVar.campoConSinDigito]:  tipoCampo => { return  new RegExp(`^${tipoCampo}(\\d{3}|)\\=.*`, `gim`)}
+  campoSinDigito:     tipoCampo => { return  new RegExp(`^${tipoCampo}\\=.*`, `gim`)},
+  campoConDigito:     tipoCampo => { return  new RegExp(`^${tipoCampo}\\d{3}\\=.*`, `gim`)},
+  campoConSinDigito:  tipoCampo => { return  new RegExp(`^${tipoCampo}(\\d{3}|)\\=.*`, `gim`)}
  // return new RegExp(`${regEx}`, `gim`)
 }
 
 exports.jsonReplace = {
-  [nomVar.addEspacioComponente]:    texto => { return texto.replace(/^\[/gm, ' \n[')},
-  [nomVar.clsCampoYContinua]:       campoIntls => { return campoIntls.replace(campo.campoYcontinua, '')},
-  [nomVar.clsComentariosIntelisis]: texto => { return texto.replace(jsonRegEx.comentarios, '')},
-  [nomVar.clsRuta]:            ruta  => { return ruta.replace(jsonRegEx.nombreArchivoEnRuta, '')},
-  [nomVar.clsSaltoLineaVacio]: texto => { return texto.replace(jsonRegEx.saltoLineaVacio, '') },
-  [nomVar.dosPuntosPorIgual]:  texto => { return texto.replace(/=/g, ':')},
-  [nomVar.extraerNombreTipoEsp]: ruta => { return ruta.replace(jsonRegEx.nombreYtipoEsp, '')},
-  [nomVar.puntoPorTipoEsp]:      nombreTipoEsp => { return nombreTipoEsp.replace(/\_(?=(frm|vis|tbl|dlg|rep))/gi, '.')},
-  [nomVar.prepararRegEx]:        texto => {
+  addEspacioCmp:    texto => { return texto.replace(/^\[/gm, ' \n[')},
+  clsCampoIntlsYContinua:       campoIntls => { return campoIntls.replace(jsonRegEx.campoIntlsYcontinua, '')},
+  clsComentariosIntls: texto => { return texto.replace(jsonRegEx.comentarios, '')},
+  clsRuta:            ruta  => { return ruta.replace(jsonRegEx.nombreArchivoEnRuta, '')},
+  clsSaltoLineaVacio: texto => { return texto.replace(jsonRegEx.saltoLineaVacio, '') },
+  dosPuntosPorIgual:  texto => { return texto.replace(/=/g, ':')},
+  extraerNombreTipoEsp: ruta => { return ruta.replace(jsonRegEx.nombreYtipoEsp, '')},
+  puntoPorGuionBajoTipoEsp:      nombreTipoEsp => { return nombreTipoEsp.replace(/\_(?=(frm|vis|tbl|dlg|rep))/gi, '.')},
+  prepararRegEx:      texto => {
     texto = texto.replace(/\[/g, '\\[').replace(/\]/g, '\\]')
     texto = texto.replace(/\(/g, '\\(').replace(/\)/g, '\\)')
     texto = texto.replace(/\{/g, '\\{').replace(/\}/g, '\\}')
@@ -52,5 +52,6 @@ exports.jsonReplace = {
     texto = texto.replace(/\./g, '\\.')
     return texto
   },
-  [saltoLineaPorComa]:    texto => { return texto.replace(jsonRegEx.saltoLinea, ', ')}
+  saltoLineaPorComaEspacio:    texto => { return texto.replace(jsonRegEx.saltoLinea, ', ')}
+  
 }
