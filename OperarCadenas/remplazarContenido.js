30a92094c85fa835af485aef2e00bd04a8c2538f
Luis A. Peña Zuñiga <lapena@grupomavi.com>
2019-01-28 14:39:50 -0600

    Correcion remplazarContenido

--- OperarCadenas/remplazarContenido.js
+++ OperarCadenas/remplazarContenido.js
@@ -1,6 +1,3 @@
-/*** Operadores de archivos***/
-const pcrArchivos =  require('../OperadoresArchivos/procesadorArchivos')
-
 /*** Operadores de cadena ***/
 const regEx       = require('../RegEx/jsonRgx')
 
@@ -10,17 +7,13 @@ const regEx       = require('../RegEx/jsonRgx')
  * @contenidoARemplazar Informacion que se quiere remplazar en @contenidoOriginal
  * @contenidoParaRemplazo Informacion para remplazar a @contenidoARemplazar
  ***/
-exports.remplazarContenido = function (contenidoOriginal, contenidoARemplazar, contenidoParaRemplazo) {
-    //let cont = 0
-    let contenidoRegEx = regEx.jsonReplace.prepararRegEx(contenidoARemplazar)
-    //pcrArchivos.crearArchivo('Testing/contenidoRegEx.txt',contenidoARemplazar)
-    let buscarContenidoARemplazar = new RegExp(`${contenidoRegEx}`, `g`)
-    // pcrArchivos.crearArchivo('Testing/contenidoRegEx-' + cont + '.txt',buscarContenidoARemplazar)
-    //pcrArchivos.crearArchivo(carpetas.carpetaTesting + 'ContenidoAchivoSinRemplazar' + cont,contenidoArchivo)
-    // pcrArchivos.crearArchivo(carpetas.carpetaTesting + 'ExpresionComponentes' + cont,buscarComponente)
-    // pcrArchivos.crearArchivo(carpetas.carpetaTesting + 'remplazoComponentes-' + cont,remplazoComponente)
-    // pcrArchivos.crearArchivo(carpetas.carpetaTesting + 'Componentes' + cont,contenidoOriginal)
-    //cont++
-    // pcrArchivos.crearArchivo(carpetas.carpetaTesting + 'nuevoContenidoArchivo',nuevoContenidoArchivo)
-    return contenidoOriginal.replace(buscarContenidoARemplazar, contenidoParaRemplazo)
+exports.remplazarContenido =  ( contenidoOriginal, contenidoARemplazar,
+                                contenidoParaRemplazo) => {
+    return  contenidoOriginal.replace(
+                new RegExp (
+                    `${regEx.jsonReplace.prepararRegEx(contenidoARemplazar)}`, `g`
+                ),
+                contenidoParaRemplazo
+            )
+    
 }
