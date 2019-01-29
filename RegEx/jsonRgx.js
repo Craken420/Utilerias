db642dcabd3fc2616e0002d56ec044d5f0282e95
Luis A. Peña Zuñiga <lapena@grupomavi.com>
2019-01-29 10:31:29 -0600

    Agregacion de expresiones de check

--- RegEx/jsonRgx.js
+++ RegEx/jsonRgx.js
@@ -1,21 +1,30 @@
 /***
  * Glosario:
  * Intls: Intelisis
- * Cmp: Componente
+ * Cmp:   Componente
+ * Diff:  Diferente
+ * Nom:   Nombre
  */
 
 const jsonRegEx = {
-  ampersand:   /\&/g, //=> &
+  ampersand:   /\&/g,   //=> &
+  //Uso check
   comentarios: /^;.*/gm, //=> ;Comentario
   campoConsecutivoIntls: /^\w+\d{3}/m, //=> Campo002, SQL002
   componentesIntls:      /\[.*?.*?[^]*?(?=\[)/g, //=> [Componente] contenidoComponente [
   campoIntlsYcontinua:   /^.*?\=<CONTINUA>|<CONTINUA>/gm, //=> SQL002 = <CONTINUA> algo <CONTINUA>
   continuaAlInicio:      /(?<=^.*?\=)<CONTINUA>/m, //=> SQL002= <CONTINUA> Algo
   continuaFinal:         /(?<=.*?)\<CONTINUA\>(\s+|)$/, //=> SQL= Algo <CONTINUA>
+  //Uso check
+  puntoExtension:        /(?<=\.)\w+$/gim,
   nombreArchivoEnRuta:   /.*\\|.*\//, //=> Busca 'c:/user/documents/ ' hasta 'Nombre Archivo.txt'
-  nombreYtipoEsp:        /.*(\/|\\)|\_MAVI.*|\.esp/gi, //De: Ruta\Achivo_FRM_MAVI.esp - Busca Achivo_FRM
+  //Uso check
+  nomExtCmp:             /(?<=\[).*?\.(frm|vis|tbl|dlg|rep)/gi,
+  //Uso check
+  nomYtipoEsp:           /.*(\/|\\)|\_MAVI.*|\.esp/gi, //De: Ruta\Achivo_FRM_MAVI.esp - Busca Achivo_FRM
   saltoLineaVacio:       /^\n[\s\t]*/gm, //=> \n\s\t Lineas vacias espacios y tabulador
-  
+  //Uso check
+  guinBajoTipoEsp:       /\_(?=(frm|vis|tbl|dlg|rep))/gi //=> Achivo_FRM
 }
 
 exports.expresiones = jsonRegEx
@@ -23,7 +32,9 @@ exports.expresiones = jsonRegEx
 exports.crearRegEx = {
   campoSinDigito:     tipoCampo => { return  new RegExp(`^${tipoCampo}\\=.*`, `gim`)},
   campoConDigito:     tipoCampo => { return  new RegExp(`^${tipoCampo}\\d{3}\\=.*`, `gim`)},
-  campoConSinDigito:  tipoCampo => { return  new RegExp(`^${tipoCampo}(\\d{3}|)\\=.*`, `gim`)}
+  campoConSinDigito:  tipoCampo => { return  new RegExp(`^${tipoCampo}(\\d{3}|)\\=.*`, `gim`)},
+  //Uso check
+  cmpDiffNomArchivo: nombreCmt => { return new RegExp(`\\[(?!\\w+;)(?!${nombreCmt})(?=.*?\\/.*?\\])[^~]*?(?=(\\n|)^\\[)`, `gim`)}
 }
 
 exports.jsonReplace = {
@@ -33,8 +44,18 @@ exports.jsonReplace = {
   clsRuta:                  ruta  => { return ruta.replace(jsonRegEx.nombreArchivoEnRuta, '')},
   clsSaltoLineaVacio:       texto => { return texto.replace(jsonRegEx.saltoLineaVacio, '') },
   dosPuntosPorIgual:        texto => { return texto.replace(/=/g, ':')},
-  extraerNombreTipoEsp:     ruta  => { return ruta.replace(jsonRegEx.nombreYtipoEsp, '')},
-  puntoPorGuionBajoTipoEsp: nombreTipoEsp => { return nombreTipoEsp.replace(/\_(?=(frm|vis|tbl|dlg|rep))/gi, '.')},
+  //Uso check
+  minusculasPorMayuscula:   texto => { return texto.replace(
+                                                      jsonRegEx.puntoExtension,
+                                                      archivoFrm => {
+                                                        return archivoFrm.toLowerCase()
+                                                      }
+                                                    )
+                                      },
+  //Uso check
+  extraerNomTipoEsp:     ruta  => { return ruta.replace(jsonRegEx.nombreYtipoEsp, '')},
+  //Uso check
+  puntoPorGuionBajoTipoEsp: nombreTipoEsp => { return nombreTipoEsp.replace(jsonRegEx.guinBajoTipoEsp, '.')},
   prepararRegEx:            texto => {
     texto = texto.replace(/\[/g,   '\\[').replace(/\]/g, '\\]')
     texto = texto.replace(/\(/g,   '\\(').replace(/\)/g, '\\)')
@@ -49,6 +70,7 @@ exports.jsonReplace = {
     texto = texto.replace(/\./g,   '\\.')
     return texto
   },
+  //Uso check
   saltoLineaPorComaEspacio:  texto => { return texto.replace(jsonRegEx.saltoLinea, ', ')}
   
 }
