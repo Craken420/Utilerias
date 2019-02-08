64638eb1df0585c5bbfb3f2eeb9d0ec5a829060f
Luis A. Peña Zuñiga <lapena@grupomavi.com>
2019-02-07 18:23:49 -0600

    Se agrego validacion de arreglo

--- OperadoresArchivos/listarArchivos.js
+++ OperadoresArchivos/listarArchivos.js
@@ -10,15 +10,18 @@ const path = require('path')
 exports.listarArchivos = (carpeta, extenciones) => {
 	let files = fs.readdirSync(carpeta)
 	if(extenciones != undefined || extenciones != null || extenciones != '') {
+		
+		if (Array.isArray(extenciones)) {
+			return archivos.filter(x => extenciones.indexOf(path.extname(x)) > -1)
+		}
+		else if (typeof extenciones == 'string') {
 
-        if (typeof extenciones == 'string') {
+			return archivos.filter(x => new RegExp(`${extenciones}`, ``).test(x))
+		} else {
 
-            return archivos.filter(x => new RegExp(`${extenciones}`, ``).test(x))
-        } else {
-
-            console.log('Ingresa un arreglo o una cadena \'.extension\'')
-        }
-    } else {
+			console.log('Ingresa un arreglo o una cadena \'.extension\'')
+		}
+	} else {
 		return files
 	}
 }
